3b2302398e09c75d470a0f80ddea7a9c17afd916
jhdz <liuyb@neusensetech.com>
2023-03-22 15:54:36 +0800

    增加测试类，从一个内容为 一段执行方法 的代码字符串中得到方法名和参数类型，即使参数也是方法

--- demo-orm-mybatis/src/test/java/com/xkcoding/orm/mybatis/reflect/MethodReflect.java
+++ demo-orm-mybatis/src/test/java/com/xkcoding/orm/mybatis/reflect/MethodReflect.java
@@ -5,8 +5,9 @@ import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MethodReflect {
 
@@ -21,7 +22,7 @@ public class MethodReflect {
             // 输入参数
             LinkedList<Object> params = new LinkedList<>();
             params.add("yyyy-MM-dd HH: mm: ss");
-            String result = (String)methodFactory(methodName, args, params);
+            String result = (String) methodFactory(methodName, args, params);
             System.out.println("输出结果" + result);
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
@@ -32,11 +33,81 @@ public class MethodReflect {
         }
     }
 
-    public Object methodFactory(String methodName, Class<?>[] args, List param) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    @Test
+    public void methodStringTest() {
+        String argTypes = getArgTypes("myMethod('abc', 123, now('yyyy-MM-dd HH:mm:ss'), dateToTimestamp('yyyy-MM-dd HH:mm:ss', now('yyyy-MM-dd HH:mm:ss')))");
+        System.out.println(argTypes.toString());
+    }
+
+    private String getArgTypes(String code) {
+        // 按照左括号分割获取方法名和参数
+        String[] tokens = new String[2];
+        tokens[0] = code.substring(0, code.indexOf('('));
+        tokens[1] = code.substring(code.indexOf('(') + 1);
+        String methodName = tokens[0].trim();
+        String argString = tokens[1].substring(0, tokens[1].length() - 1).trim();
+        // 按照逗号分割获取参数类型
+        String[] argTypes = Arrays.stream(split(argString))
+            .map(String::trim)
+            .map(arg -> {
+                if (arg.matches("^'.*'$")) {
+                    return "String";
+                } else if (arg.matches("^\\d+$")) {
+                    return "int";
+                } else if (arg.matches("^\\d+\\.\\d+$")) {
+                    return "double";
+                } else if (arg.matches("^\\d+[Ll]$")) {
+                    return "long";
+                } else if (arg.matches("^\\w+\\(.*\\)$")) {
+                    // 如果参数是一个方法，递归处理
+                    return getArgTypes(arg);
+                } else {
+                    return "Object"; // 其他类型使用Object
+                }
+            })
+            .toArray(String[]::new);
+        Map<Object, Object> resultMap = new HashMap<>();
+        resultMap.put("Method", methodName);
+        resultMap.put("Argument", Arrays.deepToString(argTypes));
+        return resultMap.toString();
+    }
+
+    public static Object methodFactory(String methodName, Class<?>[] args, List param) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         FunctionUtils functionUtils = new FunctionUtils();
         Method method = functionUtils.getClass().getMethod(methodName, args);
-        Object invoke = method.invoke(methodName,param.toArray());
+        Object invoke = method.invoke(methodName, param.toArray());
         System.out.println("调用的方法:" + methodName);
         return invoke;
+    }
+
+    public static String[] split(String str) {
+        List<String> parts = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        int depth = 0; // 记录括号的深度
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == ',' && depth == 0) { // 没有嵌套括号，直接分割
+                parts.add(builder.toString().trim());
+                builder = new StringBuilder();
+            } else {
+                builder.append(c);
+                if (c == '(') {
+                    depth++;
+                } else if (c == ')') {
+                    depth--;
+                }
+            }
+        }
+        parts.add(builder.toString().trim());
+        return parts.toArray(new String[0]);
+    }
+
+    @Test
+    public void test1() {
+        String str = "abc, 123 , now('yyyy-MM-dd HH:mm:ss', 'foo,bar'), dateToTimestamp('yyyy-MM-dd HH:mm:ss', now('yyyy-MM-dd HH:mm:ss'))";
+        String[] arr = split(str);
+        for (String s : arr) {
+            System.out.println(s);
+        }
     }
 }
